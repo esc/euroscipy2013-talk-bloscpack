bc5daee114fc5c8e87c3f44ce568a1496639a610
Valentin Haenel <valentin.haenel@gmx.de>
2013-08-16 18:35:36 +0200

    add in level configuration

--- benchmark.py
+++ benchmark.py
@@ -93,9 +93,10 @@ class AbstractRunner(object):
         del self.ndarray
         gc.collect()
 
-    def configure(self, ndarray, storage):
+    def configure(self, ndarray, storage, level):
         self.ndarray = ndarray
         self.storage = os.path.join(storage, self.filename)
+        self.level = level
 
     def ratio(self):
         return (float(os.path.getsize(self.storage)) /
@@ -114,6 +115,8 @@ class BloscpackRunner(AbstractRunner):
         self.filename = 'array.blp'
 
     def compress(self):
+        blosc_args = self.blosc_args.copy()
+        blosc_args['clevel'] = self.level
         bp.pack_ndarray_file(self.ndarray, self.storage,
                              blosc_args=self.blosc_args,
                              bloscpack_args=self.bloscpack_args)
@@ -155,7 +158,7 @@ class ZFileRunner(AbstractRunner):
         self.filename = 'array.npy'
 
     def compress(self):
-        jb.dump(self.ndarray, self.storage, compress=3, cache_size=0)
+        jb.dump(self.ndarray, self.storage, compress=self.level, cache_size=0)
 
     def decompress(self):
         it = jb.load(self.storage)
@@ -178,6 +181,12 @@ codecs = od([('bloscpack', BloscpackRunner()),
              ('zfile', ZFileRunner()),
              ])
 
+codec_levels = od([('bloscpack', [1, 3, 7, 9]),
+                   ('npz', [1, ]),
+                   ('npy', [0, ]),
+                   ('zfile', [1, 3, 7, 9]),
+                   ])
+
 columns = ['size',
            'entropy',
            'storage',
@@ -189,16 +198,23 @@ columns = ['size',
            'ratio'
            ]
 
-sets = itertools.product(dataset_sizes, storage_types, entropy_types, codecs)
+sets = []
+for size in dataset_sizes:
+    for type_ in storage_types:
+        for entropy in entropy_types:
+            for codec in codecs:
+                for level in codec_levels[codec]:
+                    sets.append((size, type_, entropy, codec, level))
+print sets
 
 #results = pd.DataFrame()
 
 for i, it in enumerate(sets):
     print it
-    size, storage, entropy, codec = it
+    size, storage, entropy, codec, level = it
     codec = codecs[codec]
     codec.configure(entropy_types[entropy](dataset_sizes[size]),
-                    storage_types[storage])
+                    storage_types[storage], level)
     print reduce(vtimeit(codec.compress, setup=codec.compress,
                  before=codec.clean, after=sync))
     print codec.ratio()
