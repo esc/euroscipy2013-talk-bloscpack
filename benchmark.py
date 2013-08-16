2897719a8d9695407e21f56c4e0e1055598ce294
Valentin Haenel <valentin.haenel@gmx.de>
2013-08-16 16:19:05 +0200

    pep8

--- benchmark.py
+++ benchmark.py
@@ -8,9 +8,8 @@ from collections import OrderedDict as od
 import numpy as np
 from numpy.random import randn
 import pandas as pd
-
-
 import bloscpack as bp
+
 
 def noop():
     pass
@@ -39,15 +38,17 @@ def vtimeit(stmt, setup=noop, before=noop, after=noop, repeat=3, number=3):
     return result
 
 
-
 def drop_caches():
     bp.drop_caches()
+
 
 def sync():
     os.system('sync')
 
+
 def make_simple_dataset(size):
     return np.arange(size)
+
 
 def make_complex_dataset(size):
     x = np.linspace(0, np.pi*16, size)
@@ -59,11 +60,14 @@ def make_complex_dataset(size):
     del noise
     return it
 
+
 def make_random_dataset(size):
     return randn(size)
 
+
 def reduce(result):
     return result.mean(axis=1).min()
+
 
 class BloscpackRunner(object):
 
@@ -95,8 +99,8 @@ class BloscpackRunner(object):
 
     def compress(self):
         bp.pack_ndarray_file(self.ndarray, self.storage,
-                blosc_args=self.blosc_args,
-                bloscpack_args=self.bloscpack_args)
+                             blosc_args=self.blosc_args,
+                             bloscpack_args=self.bloscpack_args)
 
     def decompress(self):
         it = bp.unpack_ndarray_file(self.storage)
@@ -134,11 +138,10 @@ for i, it in enumerate(sets):
     size, storage, entropy, codec = it
     codec = codecs[codec]
     codec.configure(entropy_types[entropy](dataset_sizes[size]),
-                            storage_types[storage])
+                    storage_types[storage])
     print reduce(vtimeit(codec.compress, setup=codec.compress,
-        before=codec.clean, after=sync))
+                 before=codec.clean, after=sync))
     print codec.ratio()
     codec.deconfigure()
     print reduce(vtimeit(codec.decompress, setup=codec.decompress))
     print reduce(vtimeit(codec.decompress, before=drop_caches))
-
